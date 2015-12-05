00cc1b1b1a4e5068b607d736c1fec1fb3469c606
Navid Amami <namami@pccwglobal.com>
2015-12-05 20:18:46 +0000

    file upload feature added and installed multer and hat

--- routes/ProductRegistrationRoute.js
+++ routes/ProductRegistrationRoute.js
@@ -1,16 +1,19 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
-var product = require('../models/ProductModel')(mongoose);
+var Product = require('../models/ProductModel')(mongoose);
+var multer = require('multer');
+var hat = require('hat');
+
 
 //default context root is /product
 router.post('/register', function(req, res, next) {
-  var newProduct = new product();
+  var newProduct = new Product();
   newProduct.name = req.body.name;
   newProduct.description = req.body.description;
   // newProduct.photos = req.body.photos,
   newProduct.unitsAvailable = req.body.unitsAvailable;
-  newProduct.tags = [req.body.tags];
+  newProduct.tags = req.body.tags.split(',');
   newProduct.cloverId = req.body.cloverId;
   newProduct.save(function (err, savedProduct) {
   	if(err) {
@@ -25,5 +28,43 @@ router.post('/register', function(req, res, next) {
   });
 });
 
+var uploading = multer({
+  dest: __dirname + '../public/uploads/',
+  limits: {fileSize: 1000000, files:1}
+});
+
+// TODO: ensure that the attribute name on the client side is photo
+router.post('/:productId/upload', uploading.single(hat()), function(req, res) {
+  
+  var productId = product.req.params.productId;
+  Product.findOne({_id: productId}, function(err, foundProduct) {
+        if (err) {
+          console.log(err);
+          return res.status(500).send();
+        }
+
+        if (!foundProduct) {
+          console.log('could not find product');
+          return res.status(404).send();
+        }
+
+        if (foundProduct) {
+          var productObj = res.send(foundProduct);
+          productObj.photos.push(req.files.photo.name);
+
+          productObj.save(function (err, savedProduct) {
+            if (err) {
+                console.log(err);
+                return res.status(500).send();
+            }
+
+            if (savedProduct) {
+                return res.send(savedProduct);
+            }
+            return res.status(500).send();
+          });
+        }
+    });
+});
 
 module.exports = router;
